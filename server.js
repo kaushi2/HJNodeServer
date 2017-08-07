8c59fab1643a8fb3d374b1b9b4fa292d420f05ee
Kaushal Gajjar <Kaushal.Gajjar@au.kwm.com>
2017-08-07 22:39:35 +1000

    Working Version of HotelSearch. Without Paging Tho. Becasue Travellanda does not suport paged results in xml. TODO - We have to store results in memory and server paged to Angular.

--- server.js
+++ server.js
@@ -1,56 +1,23 @@
-var qs = require("querystring");
-var express = require("express");
 var http = require("http");
+var express = require("express");
+var qs = require("querystring");
+var parseString = require('xml2js').parseString;
 
 var app = express();
 
+var host = "xmldemo.travellanda.com";
+var userName = "1d30ab517dd70cdd21c0649a0c4255fd";
+var password = "mT7C0UxRPeVn";
+var port = null;
+var path = "/xmlv1";
 
-app.get("/api/Search", function(req,res) {
-    return HotelSearch();
-});
+var header = 
+        '		<Username>USERNAME</Username>\n\t\t' +
+        '		<Password>PASSWORD</Password>\n\t\t' +
+        '		<RequestType>REQUESTTYPE</RequestType>\n\t';
 
-app.listen(3000);
-console.log("Node Server listening on 3000");
-
-function HotelSearch () {
-    var requestType = "HotelSearch";
-    var options = {
-    "method": "POST",
-    "hostname": "xmldemo.travellanda.com",
-    "port": null,
-    "path": "/xmlv1",
-    "headers": {
-        "requesttype": requestType,
-        "authorization": "Basic MWQzMGFiNTE3ZGQ3MGNkZDIxYzA2NDlhMGM0MjU1ZmQ6bVQ3QzBVeFJQZVZu",
-        "cache-control": "no-cache",
-        "postman-token": "6704f625-3148-6732-f497-ae5007b32b48",
-        "content-type": "application/x-www-form-urlencoded"
-    }
-    };
-
-    var req = http.request(options, function (res) {
-        var chunks = [];
-
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
-            return body.toString();
-        });
-    });
-
-    req.write(qs.stringify({ xml: 
-        '<?xml version="1.0" encoding="UTF-8"?>\n' +
-        ' <Request>\n\t' +
-        '	<Head>\n\t\t' +
-        '		<Username>1d30ab517dd70cdd21c0649a0c4255fd</Username>\n\t\t' +
-        '		<Password>mT7C0UxRPeVn</Password>\n\t\t' +
-        '		<RequestType>' + requestType + '</RequestType>\n\t' +
-        '	</Head>\n\t' +
-        '	<Body>\n\t\t' +
+app.get("/api/Search", function(req,response) {
+    var HotelSearchBody = 
         '		<CityIds>\n\t\t\t' +
         '			<CityId>117976</CityId>\n\t\t' +
         '		</CityIds>\n\t\t' +
@@ -58,7 +25,7 @@ function HotelSearch () {
         '		<CheckOutDate>2017-12-20</CheckOutDate>\n\t\t' +
         '		<Rooms>\n\t\t\t' +
         '			<Room>\n\t\t\t\t' +
-        '				<NumAdults>2</NumAdults>\n\t\t\t' +
+        '				<NumAdults>4</NumAdults>\n\t\t\t' +
         '			</Room>\n\t\t\t' +
         '			<Room>\n\t\t\t\t' +
         '				<NumAdults>1</NumAdults>\n\t\t\t\t' +
@@ -70,8 +37,75 @@ function HotelSearch () {
         '		</Rooms>\n\t\t' +
         '		<Nationality>FR</Nationality>\n\t\t' +
         '		<Currency>GBP</Currency>\n\t\t' +
-        '		<AvailableOnly>0</AvailableOnly>\n\t' +
+        '		<AvailableOnly>1</AvailableOnly>\n\t';
+
+    performRequest('', 'POST', "HotelSearch", HotelSearchBody, function(data) {
+        //console.log('Fetched ' + data.result.paging.total_items + ' Hotels');
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(data);
+        response.end();
+        
+    });
+});
+
+app.listen(3000);
+console.log("Node Server listening on 3000");
+
+function performRequest(endpoint, method, requestType, body, success) {
+    header = header.replace("REQUESTTYPE", requestType);
+    header = header.replace("USERNAME", userName);
+    header = header.replace("PASSWORD", password);
+    var dataString = qs.stringify({ xml: 
+        '<?xml version="1.0" encoding="UTF-8"?>\n' +
+        ' <Request>\n\t' +
+        '	<Head>\n\t\t' + 
+                header +
+        '	</Head>\n\t' +
+        '	<Body>\n\t\t' +
+                body +
         '	</Body>\n' +
-        '</Request>' }));
+        '</Request>' });
+  
+    var headers = {};
+  
+    if (method == 'GET') {
+        endpoint += '?' + querystring.stringify(data);
+    }
+    else {
+        headers = {
+            'requesttype': requestType,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(dataString)
+        };
+    }
+    var options = {
+        host: host,
+        path: path,
+        method: method,
+        headers: headers
+    };
+
+    var req = http.request(options, function(res) {
+        res.setEncoding('utf-8');
+
+        var responseString = '';
+
+        res.on('data', function(data) {
+            responseString += data;
+        });
+
+        res.on('end', function() {
+            //console.log(responseString);
+            parseString(responseString, {explicitArray: false}, function (err, result) {
+                //console.dir(JSON.stringify(result));
+                success(JSON.stringify(result));
+            });
+            //success(responseObject);
+        });
+    });
+
+    req.write(dataString);
     req.end();
 }
+
+
